7cd2f574c8cc2aa76b446ca2f24e1a9bcb229d50
bea4dev <bea0224@outlook.jp>
2024-05-26 02:59:50 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     testImplementation(kotlin("test"))
 
     // Minestom
-    implementation("com.github.bea4dev:Minestom:c2dc171bc1")
+    implementation("com.github.bea4dev:Minestom:46da6b5f20")
 
     // Log
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
